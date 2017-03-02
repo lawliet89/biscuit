10786d500080006c637080cc305e2a5de27326bd
Yong Wen Chua <me@yongwen.xyz>
2017-03-02 17:02:10 +0800

    Add claims

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 // #![warn(missing_docs)]
 #![doc(test(attr(allow(unused_variables), deny(warnings))))]
 
+extern crate chrono;
 extern crate rustc_serialize;
 extern crate ring;
 extern crate serde;
@@ -14,6 +15,7 @@ extern crate untrusted;
 
 use std::fmt;
 
+use chrono::{DateTime, UTC};
 use rustc_serialize::base64::{self, ToBase64, FromBase64};
 use serde::{Serialize, Serializer, Deserialize, Deserializer};
 use serde::ser::{SerializeSeq, SerializeStruct};
@@ -50,6 +52,140 @@ impl<T> Part for T
         let decoded = encoded.as_ref().from_base64()?;
         let s = String::from_utf8(decoded)?;
         Ok(serde_json::from_str(&s)?)
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum SingleOrMultipleStrings {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+impl Serialize for SingleOrMultipleStrings {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        use SingleOrMultipleStrings::*;
+        match *self {
+            Single(ref value) => serializer.serialize_str(value),
+            Multiple(ref values) => {
+                let mut seq = serializer.serialize_seq(Some(values.len()))?;
+                for element in values {
+                    seq.serialize_element(element)?;
+                }
+                seq.end()
+            }
+        }
+    }
+}
+
+impl Deserialize for SingleOrMultipleStrings {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+        where D: Deserializer
+    {
+        struct SingleOrMultipleStringsVisitor;
+
+        impl Visitor for SingleOrMultipleStringsVisitor {
+            type Value = SingleOrMultipleStrings;
+
+            fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+                write!(formatter, "a string or an array of strings")
+            }
+
+            fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
+                where E: de::Error
+            {
+                Ok(SingleOrMultipleStrings::Single(s.to_string()))
+            }
+
+            fn visit_string<E>(self, s: String) -> Result<Self::Value, E>
+                where E: de::Error
+            {
+                Ok(SingleOrMultipleStrings::Single(s.to_string()))
+            }
+
+            fn visit_seq<V>(self, mut visitor: V) -> Result<Self::Value, V::Error>
+                where V: de::SeqVisitor
+            {
+                let capacity_hint = match visitor.size_hint() {
+                    (lower, None) => lower,
+                    (_, Some(upper)) => upper,
+                };
+
+                let mut strings: Vec<String> = Vec::with_capacity(capacity_hint);
+                while let Ok(Some(string)) = visitor.visit::<String>() {
+                    strings.push(string);
+                }
+                Ok(SingleOrMultipleStrings::Multiple(strings))
+            }
+        }
+
+        deserializer.deserialize_map(SingleOrMultipleStringsVisitor {})
+    }
+}
+
+pub struct ClaimsSet<T: Serialize + Deserialize> {
+    registered: RegisteredClaims,
+    private: T,
+}
+
+#[derive(Debug, Eq, PartialEq, Deserialize)]
+// TODO: TEST!
+pub struct RegisteredClaims {
+    // Issuer
+    pub iss: Option<String>,
+    /// Subject
+    pub sub: Option<String>,
+    /// Audience
+    pub aud: Option<SingleOrMultipleStrings>,
+    /// Expiration time
+    pub exp: Option<DateTime<UTC>>,
+    /// Not before time
+    pub nbf: Option<DateTime<UTC>>,
+    /// Issued at Time
+    pub iat: Option<DateTime<UTC>>,
+    /// JWT ID
+    pub jti: Option<String>,
+}
+
+impl RegisteredClaims {
+    fn count_fields(&self) -> usize {
+        macro_rules! count {
+            ($field_name:ident) => (
+                match self.$field_name {
+                    Some(_) => 1,
+                    None => 0
+                }
+            )
+        }
+
+        count!(iss) + count!(sub) + count!(aud) + count!(exp) + count!(nbf) + count!(iat) + count!(jti)
+    }
+}
+
+impl Serialize for RegisteredClaims {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where S: Serializer
+    {
+        let mut struc = serializer.serialize_struct("RegisteredClaims", self.count_fields())?;
+
+        macro_rules! optional {
+            ($field_name:ident) => (
+                if let Some(ref value) = self.$field_name {
+                    struc.serialize_field(stringify!($field_name), value)?;
+                }
+            )
+        }
+
+        optional!(iss);
+        optional!(sub);
+        optional!(aud);
+        optional!(exp);
+        optional!(nbf);
+        optional!(iat);
+        optional!(jti);
+
+        struc.end()
     }
 }
 
@@ -113,13 +249,44 @@ mod tests {
     use std::str;
     use serde_json;
 
-    use super::{encode, decode};
+    use super::{encode, decode, SingleOrMultipleStrings};
     use jws::{Algorithm, Header};
 
     #[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
     struct Claims {
         sub: String,
         company: String,
+    }
+
+    #[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+    struct SingleOrMultipleStringsTest {
+        values: SingleOrMultipleStrings,
+    }
+
+    #[test]
+    fn single_string_serialization_round_trip() {
+        let test = SingleOrMultipleStringsTest { values: SingleOrMultipleStrings::Single("foobar".to_string()) };
+        let expected_json = r#"{"values":"foobar"}"#;
+
+        let serialized = not_err!(serde_json::to_string(&test));
+        assert_eq!(expected_json, serialized);
+
+        let deserialized: SingleOrMultipleStringsTest = not_err!(serde_json::from_str(&serialized));
+        assert_eq!(deserialized, test);
+    }
+
+    #[test]
+    fn multiple_strings_serialization_round_trip() {
+        let test = SingleOrMultipleStringsTest {
+            values: SingleOrMultipleStrings::Multiple(vec!["foo".to_string(), "bar".to_string(), "baz".to_string()]),
+        };
+        let expected_json = r#"{"values":["foo","bar","baz"]}"#;
+
+        let serialized = not_err!(serde_json::to_string(&test));
+        assert_eq!(expected_json, serialized);
+
+        let deserialized: SingleOrMultipleStringsTest = not_err!(serde_json::from_str(&serialized));
+        assert_eq!(deserialized, test);
     }
 
     #[test]
