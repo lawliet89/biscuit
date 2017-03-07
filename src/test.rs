e0e1895751504db448e918bf01720bc6da874b6c
Yong Wen Chua <lawliet89@users.noreply.github.com>
2017-03-07 12:21:28 +0800

    ECDSA signature verification (#11)
    * Add unsupported guards to ECDSA signing
    
    * Add verification tests
    
    * More tests
    
    * Fix typo

--- src/test.rs
+++ src/test.rs
@@ -8,11 +8,11 @@ macro_rules! not_err {
     })
 }
 
-pub fn read_private_key() -> signature::RSAKeyPair {
-    not_err!(signature::RSAKeyPair::from_der(untrusted::Input::from(include_bytes!("../test/fixtures/private_key.\
+pub fn read_rsa_private_key() -> signature::RSAKeyPair {
+    not_err!(signature::RSAKeyPair::from_der(untrusted::Input::from(include_bytes!("../test/fixtures/rsa_private_key.\
                                                                                     der"))))
 }
 
-pub fn read_public_key() -> Vec<u8> {
-    include_bytes!("../test/fixtures/public_key.der").iter().map(|b| b.clone()).collect()
+pub fn read_rsa_public_key() -> Vec<u8> {
+    include_bytes!("../test/fixtures/rsa_public_key.der").to_vec()
 }
