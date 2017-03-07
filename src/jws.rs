fd7558070e78e4bba8f177aaa1e61ee8f71ede22
Yong Wen Chua <me@yongwen.xyz>
2017-03-07 16:44:26 +0800

    Document the algorithms

--- src/jws.rs
+++ src/jws.rs
@@ -148,21 +148,36 @@ impl Default for Header {
 }
 
 #[derive(Debug, PartialEq, Copy, Clone, Serialize, Deserialize)]
-/// The algorithms supported for signing/verifying
+/// The algorithms supported for signing/verifying.
 // TODO: Add support for `none`
 pub enum Algorithm {
+    /// HMAC using SHA-256
     HS256,
+    /// HMAC using SHA-384
     HS384,
+    /// HMAC using SHA-512
     HS512,
+    /// RSASSA-PKCS1-v1_5 using SHA-256
     RS256,
+    /// RSASSA-PKCS1-v1_5 using SHA-384
     RS384,
+    /// RSASSA-PKCS1-v1_5 using SHA-512
     RS512,
+    /// ECDSA using P-256 and SHA-256
     ES256,
+    /// ECDSA using P-384 and SHA-384
     ES384,
-    /// This variant is [unsupported](https://github.com/briansmith/ring/issues/268) and will probably never be
+    /// ECDSA using P-521 and SHA-512 --
+    /// This variant is [unsupported](https://github.com/briansmith/ring/issues/268) and will probably never be.
     ES512,
+    /// RSASSA-PSS using SHA-256 and MGF1 with SHA-256.
+    /// The size of the salt value is the same size as the hash function output.
     PS256,
+    /// RSASSA-PSS using SHA-384 and MGF1 with SHA-384
+    /// The size of the salt value is the same size as the hash function output.
     PS384,
+    /// RSASSA-PSS using SHA-512 and MGF1 with SHA-512
+    /// The size of the salt value is the same size as the hash function output.
     PS512,
 }
 
