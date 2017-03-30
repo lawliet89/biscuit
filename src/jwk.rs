daeb8e00cb6f3d824dee565563324c4a1103c6e7
Yong Wen Chua <me@yongwen.xyz>
2017-03-30 14:43:15 +0800

    Fix tests

--- src/jwk.rs
+++ src/jwk.rs
@@ -365,11 +365,11 @@ mod tests {
             additional: Default::default()
         };
         let expected_json = r#"{
-    "kty":"EC",
-    "crv":"P-256",
-    "x":"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
-    "y":"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
-    "kid":"Public key used in JWS spec Appendix A.3 example"
+  "kid": "Public key used in JWS spec Appendix A.3 example",
+  "kty": "EC",
+  "crv": "P-256",
+  "x": "f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
+  "y": "x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0"
 }"#;
 
         let serialized = not_err!(serde_json::to_string_pretty(&test_value));
