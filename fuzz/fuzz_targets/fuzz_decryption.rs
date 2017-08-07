811e5f98fcb6c6f2a55fd43efd02bd7e0ef43997
Ng Guoyou <ng.guoyou@gmail.com>
2017-08-07 16:27:11 +0800

    Add basic fuzzing for decryption and signature validation

---
+++ fuzz/fuzz_targets/fuzz_decryption.rs
@@ -0,0 +1,27 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate biscuit;
+extern crate serde_json;
+
+use biscuit::{Empty, JWE};
+use biscuit::jwk::JWK;
+use biscuit::jwa::{KeyManagementAlgorithm, ContentEncryptionAlgorithm};
+
+fuzz_target!(|data: &[u8]| {
+    let key: JWK<Empty> = JWK::new_octect_key(&vec![0; 256 / 8], Default::default());
+
+    let token = std::str::from_utf8(data);
+    if token.is_err() {
+        return;
+    }
+    let token = token.unwrap();
+
+    let token: JWE<serde_json::Value, biscuit::Empty, biscuit::Empty> = JWE::new_encrypted(&token);
+
+    let _ = token.into_decrypted(
+        &key,
+        KeyManagementAlgorithm::A256GCMKW,
+        ContentEncryptionAlgorithm::A256GCM,
+    );
+});
---
+++ fuzz/fuzz_targets/fuzz_signature_validation.rs
@@ -0,0 +1,23 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+extern crate biscuit;
+extern crate serde_json;
+
+use std::str::FromStr;
+use biscuit::*;
+use biscuit::jws::*;
+use biscuit::jwa::*;
+
+fuzz_target!(|data: &[u8]| {
+    let signing_secret = Secret::Bytes("secret".to_string().into_bytes());
+
+    let expected_token = std::str::from_utf8(data);
+    if expected_token.is_err() {
+        return;
+    }
+    let expected_token = expected_token.unwrap();
+
+    let token = JWT::<serde_json::Value, biscuit::Empty>::new_encoded(&expected_token);
+    let _ = token.into_decoded(&signing_secret, SignatureAlgorithm::HS256);
+});
