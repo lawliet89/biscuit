f6d813297d003c5bf535f72ee72c97b51497eea9
Gabriel de Perthuis <g2p.code@gmail.com>
2020-07-15 20:51:58 +0200

    Add a test for badly-normalized values
    This is for the RSA exponent case; other members / key types have not
    been checked.

--- src/jwk.rs
+++ src/jwk.rs
@@ -1403,6 +1403,26 @@ mod tests {
     }
 
     #[test]
+    fn jwk_rsa_thumbprint_normalization_gotcha() {
+        // Modified from https://tools.ietf.org/html/rfc7638#section-3.1
+        // to exemplify a gotcha from  https://tools.ietf.org/html/rfc7638#section-7
+        let jwk: JWK<Empty> = serde_json::from_str(
+            r#"{
+            "kty": "RSA",
+            "n": "0vx7agoebGcQSuuPiLJXZptN9nndrQmbXEps2aiAFbWhM78LhWx4cbbfAAtVT86zwu1RK7aPFFxuhDR1L6tSoc_BJECPebWKRXjBZCiFV4n3oknjhMstn64tZ_2W-5JsGY4Hc5n9yBXArwl93lqt7_RN5w6Cf0h4QyQ5v-65YGjQR0_FDW2QvzqY368QQMicAtaSqzs8KJZgnYb9c7d0zgdAZHzu6qMQvRL5hajrn1n91CbOpbISD08qNLyrdkt-bFTWhAI4vMQFh6WeZu0fM4lFd2NcRwr3XPksINHaQ-G_xBniIqbw0Ls1jF44-csFCur-kEgU8awapJzKnqDKgw",
+            "e": "AAEAAQ",
+            "alg": "RS256",
+            "kid": "2011-04-29"
+           }"#,
+        )
+        .unwrap();
+        assert_eq!(
+            jwk.algorithm.thumbprint(&ring::digest::SHA256).unwrap(),
+            "NzbLsXh8uDCcd-6MNwXF4W_7noWXFZAfHkxZsRGC9Xs"
+        );
+    }
+
+    #[test]
     fn jwk_oct_thumbprint() {
         let jwk: JWK<Empty> = serde_json::from_str(
             r#"{
