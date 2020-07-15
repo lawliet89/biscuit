6ac6f1b9a1a1b347031c93e151f46f41793ddb58
Gabriel de Perthuis <g2p.code@gmail.com>
2020-07-15 20:39:39 +0200

    Flattened JWS
    We support flattened JWS minus unprotected headers.
    
    Added tests, most ported from compact JWS and some new ones.

--- src/jws.rs
+++ src/jws.rs
@@ -3,5 +3,7 @@
 //! Defined in [RFC 7515](https://tools.ietf.org/html/rfc7515). For most common use,
 //! you will want to look at the  [`Compact`](enum.Compact.html) enum.
 mod compact;
+mod flattened;
 
 pub use compact::{Compact, Header, RegisteredHeader, Secret};
+pub use flattened::{Signable, SignedData};
